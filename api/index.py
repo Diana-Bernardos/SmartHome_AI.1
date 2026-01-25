37efff1958a9f70c2da8add01ca190153b20865f
Diana-Bernardos <dianilla75@gmail.com>
2026-01-25 22:09:30 +0100

    3.4

--- api/index.py
+++ api/index.py
@@ -1,4 +1,9 @@
+import sys
+import os
+
+# Add the parent directory to sys.path so we can import 'api' from root
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from api import app
 
 # This is the entry point for Vercel serverless functions
-# It exposes the FastAPI app instance
