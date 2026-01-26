7da4d6cb1725dbe827ab0ebd11c3e6770ea66ba3
Diana-Bernardos <dianilla75@gmail.com>
2026-01-26 09:38:43 +0100

    404

--- api/index.py
+++ api/index.py
@@ -4,6 +4,6 @@ import os
 # Add the parent directory to sys.path so we can import 'api' from root
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from api import app
+from backend import app
 
 # This is the entry point for Vercel serverless functions
